9414874b14bb87e8cff205bc4f5b26dfb591abb0
Juan Carlos Collao Mamani <jccm0909@gmail.com>
2025-06-16 10:24:46 -0400

    Corrigiendo bugs de estilos pagina 404

--- views/error/404.php
+++ views/error/404.php
@@ -1,6 +1,6 @@
-<div class="error">
-    <h1 class="error__titulo">PAGINA 404</h1>
-    <P class="error__parrafo">Lo sentimos! 😢 la pagina solicitada no se encuentra en el servidor</P>
+<div class="error404">
+    <h1 class="error404__titulo">PAGINA 404</h1>
+    <P class="error404__parrafo">Lo sentimos! 😢 la pagina solicitada no se encuentra en el servidor</P>
 
-    <a class="error__enlace" href="/admin/dashboard">Volver al inicio</a>
+    <a class="error404__enlace" href="/admin/dashboard">Volver al inicio</a>
 </div>
